e6f5ab99f5f26980ba7847f507d000e418b61ff1
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-07-23 13:36:36 +0900

    ♻️ Ref: 현재 경로가 Home인지 Favorites인지에 따라 다르게 렌더링되도록 수정

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,9 +1,10 @@
-import { Box, Button, Card, Flex, Image } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, Card, Flex, Image } from '@chakra-ui/react';
 import { IoMdHeart } from 'react-icons/io';
+import { IoMdHeartDislike } from 'react-icons/io';
 
-function RestaurantCard() {
+function RestaurantCard({ label }) {
   return (
-    <Card.Root colorPalette="yellow">
+    <Card.Root>
       <Flex direction="column" align="center">
         <Image
           src="https://cdn.pixabay.com/photo/2021/07/20/06/04/restaurant-6479818_960_720.jpg"
@@ -13,10 +14,17 @@ function RestaurantCard() {
           <Card.Title>Example Restaurant</Card.Title>
         </Card.Body>
         <Card.Footer>
-          <Button variants="surface">
-            <IoMdHeart />
-            Add Favorites
-          </Button>
+          {label === 'home' ? (
+            <Button colorPalette="yellow" variants="surface">
+              <IoMdHeart />
+              Add Favorites
+            </Button>
+          ) : (
+            <Button colorPalette="red" variants="surface">
+              <IoMdHeartDislike />
+              Remove
+            </Button>
+          )}
         </Card.Footer>
       </Flex>
     </Card.Root>
