13f7fd203c043b2f1dcb915ff7df638c1a186caf
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-07-23 23:20:33 +0900

    ♻️ Ref: main에 Toaster 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { Provider } from '@components/ui/provider.jsx';
 import { BrowserRouter } from 'react-router';
+import { Toaster } from './components/ui/toaster.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider>
+      <Toaster />
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
   </StrictMode>
 );
-
