6e7738f00cf9144391cbfe80d925e8e314ca586a
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-07-23 13:20:40 +0900

    ♻️ Ref: Home에 검색바, 레스토랑 정보 카드 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,38 @@
+import Card from '@/components/RestaurantCard';
+import { Box, Center, Flex, Grid, Heading, Input, InputGroup } from '@chakra-ui/react';
+import { LuSearch } from 'react-icons/lu';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Center>
+      <Box width="75%" py="10">
+        <Flex direction="column" gap="12">
+          <Flex direction="column" gap="4">
+            <Heading size="3xl">All Restaurants</Heading>
+            <InputGroup startElement={<LuSearch color="#9E8747" />}>
+              <Input
+                variant="subtle"
+                bg="#F4F0E6"
+                _placeholder={{ color: '#9E8747' }}
+                placeholder="search for restaurants"
+              />
+            </InputGroup>
+          </Flex>
+
+          <Grid templateColumns="repeat(3, 1fr)" gap="4">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </Grid>
+        </Flex>
+      </Box>
+    </Center>
+  );
 }
 
 export default Home;
