8ae3f8c2d5295e9b2fdbd9ab46204feedc1e42bd
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-07-23 12:08:28 +0900

    💄 Design: 배경색 추가

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,13 +1,14 @@
+import { Box } from '@chakra-ui/react';
 import Header from '@components/Header';
 import { Outlet } from 'react-router';
 
 function Layout() {
   return (
-    <div>
+    <Box bg="#fcfaf7" minHeight="100vh">
       <Header />
       <Outlet />
       {/* Footer */}
-    </div>
+    </Box>
   );
 }
 
