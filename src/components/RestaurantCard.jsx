c823f5adcb55190054926f26eb78cfa403f88f54
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-07-24 16:28:32 +0900

    ♻️ Ref: useDialog를 적용해서 좋아요 삭제할 때 dialog 렌더링
    외부에 있는 버튼을 사용할 때는 useDialog를 사용해야 함

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,30 +1,50 @@
-import { Button, Card, Flex, Image } from '@chakra-ui/react';
+import { Button, Card, Dialog, Flex, Image, useDialog, useDisclosure } from '@chakra-ui/react';
 import { IoMdHeart } from 'react-icons/io';
 import { IoMdHeartDislike } from 'react-icons/io';
+import ConfirmDialog from './ConfirmDialog';
 
 function RestaurantCard({ id, title, image, label, handleFavoriteAdd, handleFavoriteRemove }) {
+  const dialog = useDialog();
+
   return (
-    <Card.Root>
-      <Flex direction="column" align="center">
-        <Image aspectRatio={4 / 3} src={`http://localhost:3000/${image.src}`} alt={image.alt} />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-        </Card.Body>
-        <Card.Footer>
-          {label === 'home' ? (
-            <Button colorPalette="yellow" variants="surface" onClick={() => handleFavoriteAdd(id)}>
-              <IoMdHeart />
-              Add Favorites
-            </Button>
-          ) : (
-            <Button colorPalette="red" variants="surface" onClick={() => handleFavoriteRemove(id)}>
-              <IoMdHeartDislike />
-              Remove
-            </Button>
-          )}
-        </Card.Footer>
-      </Flex>
-    </Card.Root>
+    <>
+      <Card.Root>
+        <Flex direction="column" align="center">
+          <Image aspectRatio={4 / 3} src={`http://localhost:3000/${image.src}`} alt={image.alt} />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+          </Card.Body>
+          <Card.Footer>
+            {label === 'home' ? (
+              <Button
+                colorPalette="yellow"
+                variants="surface"
+                onClick={() => handleFavoriteAdd(id)}
+              >
+                <IoMdHeart />
+                Add Favorites
+              </Button>
+            ) : (
+              <Dialog.RootProvider value={dialog} role="alertdialog">
+                <Dialog.Trigger asChild>
+                  <Button colorPalette="red" variants="surface">
+                    <IoMdHeartDislike />
+                    Remove
+                  </Button>
+                </Dialog.Trigger>
+                <ConfirmDialog
+                  dialog={dialog}
+                  onConfirm={() => handleFavoriteRemove(id)}
+                  title="정말 삭제하시겠습니까?"
+                >
+                  Favorites 목록에서 맛집이 삭제됩니다.
+                </ConfirmDialog>
+              </Dialog.RootProvider>
+            )}
+          </Card.Footer>
+        </Flex>
+      </Card.Root>
+    </>
   );
 }
 
